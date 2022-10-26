6026f392978ee1de9231d7475dc17871a2ccae6e
Dms Fahim Patoawary <dmsfahim@gmail.com>
2022-10-26 19:02:02 +0600

    1sst comit

---
+++ main.js
@@ -0,0 +1,11 @@
+function juice_img(image){
+  let main_img = document.querySelector('.main').src = image;
+}
+
+$(document).ready(function(){
+  $("#zoom").imagezoomsl();
+})
+// $(function(){
+//     $("#zoom").imagezoomsl();
+// });
+  
