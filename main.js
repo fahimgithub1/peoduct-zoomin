b881987d78a9b7cafba636f6f34eaa2e81a5c499
Dms Fahim Patoawary <dmsfahim@gmail.com>
2022-10-27 12:28:41 +0600

    finely done

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ function juice_img(image){
   let main_img = document.querySelector('.main').src = image;
 }
 
-$(document).ready(function(){
-  $("#zoom").imagezoomsl();
-})
-// $(function(){
-//     $("#zoom").imagezoomsl();
-// });
-  
+// $(document).ready(function(){
+//   $("#zoom").imagezoomsl();
+// })
